1194c8c0a98b55d42a542d61bf72ee5479217c6b
Tusar Singh <rts0941@gmail.com>
2024-08-02 14:48:03 +0530

    Create a Project Architecture ' Combination of Some Project ' for Web Application

---
+++ Radhey/Radhey.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Radhey.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
